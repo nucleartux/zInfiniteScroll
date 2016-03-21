e5ff0c58c0b2a09e4cd9bceb9e95bebfabd7caec
Adrov Igor <nucleartux@gmail.com>
2016-03-21 13:00:48 +0300

    update

--- zInfiniteScroll.js
+++ zInfiniteScroll.js
@@ -2,10 +2,9 @@
     'use strict';
     var module = ng.module('zInfiniteScroll', []);
 
-    module.directive('zInfiniteScroll', ['$timeout', '$document', function ($timeout, $document) {
+    module.directive('zInfiniteScroll', ['$timeout', '$interval', '$document', function ($timeout, $interval, $document) {
         return {
-            restrict: 'A',
-            link: function link($scope, $element, $attr) {
+            link: function ($scope, $element, $attr) {
                 var lengthThreshold = $attr.scrollThreshold || 50,
                     timeThreshold   = $attr.timeThreshold || 200,
                     handler         = $scope.$eval($attr.zInfiniteScroll),
@@ -19,7 +18,6 @@
 
                 $scope.$on('$destroy', function handleDestroyEvent() {
                     isDestorying = true;
-                    $document.off('scroll', scrollEvent);
                 });
 
                 lengthThreshold = parseInt(lengthThreshold, 10);
@@ -37,23 +35,62 @@
                     scrollEvent = scrollUntilTimeout;
                 }
 
+                var throttle = function(func, wait) {
+                  var later, previous, timeout;
+                  timeout = null;
+                  previous = 0;
+                  later = function() {
+                    var context;
+                    previous = new Date().getTime();
+                    $interval.cancel(timeout);
+                    timeout = null;
+                    func.call();
+                    return context = null;
+                  };
+                  return function() {
+                    var now, remaining;
+                    now = new Date().getTime();
+                    remaining = wait - (now - previous);
+                    if (remaining <= 0) {
+                      clearTimeout(timeout);
+                      $interval.cancel(timeout);
+                      timeout = null;
+                      previous = now;
+                      return func.call();
+                    } else {
+                      if (!timeout) {
+                        return timeout = $interval(later, remaining, 1);
+                      }
+                    }
+                  };
+                };
+
+                if(timeThreshold) {
+                    scrollEvent = throttle(scrollEvent, timeThreshold);
+                }
+
                 // if element doesn't want to set height, this would be helpful.
                 if (bodyScroll) {
-                    $document.on('scroll', scrollEvent);
-                    element = $document[0].documentElement;
+                    $document.bind('scroll', scrollEvent);
+                    element = $document[0].body;
                 } else {
-                    $element.on('scroll', scrollEvent);
+                    $element.bind('scroll', scrollEvent);
                 }
+
+                // scroll first to the bottom (with a delay so the elements are rendered)
+                $timeout(function() {
+                    element.scrollTop = element.clientHeight;
+                }, 0);
 
                 // it will be scrolled once your data loaded
                 function scrollUntilDataReady() {
                     if (isDestorying) return;
+                    var scrolled = inverse ? element.scrollTop : element.scrollHeight - (element.clientHeight + element.scrollTop);
 
-                    var scrolled = calculateBarScrolled();
                     // if we have reached the threshold and we scroll up
-                    if (scrolled < lengthThreshold && (scrolled - lastScrolled) < 0 && (element.scrollHeight >= element.clientHeight)) {
+                    if (scrolled < lengthThreshold && (scrolled - lastScrolled) < 0) {
                         var originalHeight = element.scrollHeight;
-                        $scope.$apply(handler).then(function() {
+                        handler().then(function() {
                             $timeout(function() {
                                 element.scrollTop = element.scrollHeight - originalHeight;
                             });
@@ -62,12 +99,13 @@
                     lastScrolled = scrolled;
                 }
 
+                // this need you set the div height
                 function scrollUntilTimeout() {
                     if (isDestorying) return;
-                    var scrolled = calculateBarScrolled();
+                    var scrolled = inverse ? element.scrollTop : element.scrollHeight - (element.clientHeight + element.scrollTop);
 
                     // if we have reached the threshold and we scroll down
-                    if (scrolled < lengthThreshold && (scrolled - lastScrolled) < 0 && (element.scrollHeight >= element.clientHeight)) {
+                    if (scrolled < lengthThreshold && (scrolled - lastScrolled) < 0) {
                         // if there is already a timer running which has no expired yet we have to cancel it and restart the timer
                         if (promise !== null) {
                             $timeout.cancel(promise);
@@ -78,17 +116,6 @@
                         }, timeThreshold);
                     }
                     lastScrolled = scrolled;
-                }
-
-                // for compatibility for all browser
-                function calculateBarScrolled() {
-                    var scrollTop;
-                    if (bodyScroll) {
-                        scrollTop = $document[0].documentElement.scrollTop || $document[0].body.scrollTop;
-                    } else {
-                        scrollTop = element.scrollTop;
-                    }
-                    return inverse ? scrollTop : element.scrollHeight - (element.clientHeight + scrollTop);
                 }
             }
         };
